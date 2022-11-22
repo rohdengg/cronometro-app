8e7851cb69c0341afed484f365acc20670016963
rohdengg <rohdengg@gmail.com>
2022-11-22 10:42:24 -0300

    functional stopwatch

--- App.js
+++ App.js
@@ -1,10 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextComponent, View } from 'react-native';
 
 export default function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [customInterval, setCustomInterval] = useState();
+  const [isActive, setisActive] = useState(false);
+
+
+  const startTimer = () => {
+    if (!isActive) {
+      setCustomInterval(
+        setInterval(() => {
+          changeTime();
+        }, 1000)
+      )
+      setisActive(true)
+    }
+  };
+
+  function stopTimer() {
+    if (customInterval) {
+      clearInterval(customInterval);
+      setisActive(false)
+    }
+  }
+
+  const clear = () => {
+    stopTimer();
+    setSeconds(0);
+    setMinutes(0);
+  };
+
+  const changeTime = () => {
+    setSeconds((prevState) => {
+      if (prevState + 1 == 60) {
+        setMinutes((prevState) =>{
+          return prevState + 1;
+        });
+        return 0;
+      }
+      return prevState +1;
+    });
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textTimer}>
+        {minutes < 10 ? '0' + minutes : minutes}:
+        {seconds < 10 ? '0' + seconds : seconds}
+      </Text>
+      <View style={styles.buttonContainer}>
+        {isActive ? (
+          <Button title='Stop' onPress={stopTimer}/>
+        ) : (
+          <Button title='Start' onPress={startTimer}/>
+        )}
+        
+        
+        <Button title='clear' onPress={clear}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +73,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textTimer: {
+    fontSize: 60,
+  },
+  buttonContainer: {
+    width: "50%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
   },
 });
